5fa40e23eb419eca3a36662fbdf7181c29781982
JayDub21 <justin.wofford.21@gmail.com>
2020-01-28 19:22:46 -0600

    basic sql file setup

---
+++ company_db.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS company_db;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE departments (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary DECIMAL (9, 2) NOT NULL,
+  department_id INT NOT NULL
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR (30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+
+SELECT * FROM departments;
+
+SELECT * FROM role;
+
+SELECT * FROM employee;
+
